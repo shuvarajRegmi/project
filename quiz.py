34c727fe89faf5453e11ab8da81a13ddf755166e
shuvarajRegmi <shuvarajregmi10@gmail.com>
2025-05-08 23:33:02 +0545

    quiz

---
+++ quiz.py
@@ -0,0 +1,32 @@
+print("Welcome to my computer quiz!")
+x=input("Do you want to paly? ")
+if x !="yes":
+    quit()
+print("Okay! lets play:)")
+answer=input("What does CPU stand for?")
+if answer=="Central processing unit":
+    print("correct!")
+else:
+    print("incorrect")
+answer=input("Which country has the highest life expectancy?")
+if answer=="Japan":
+    print("correct!")
+else:
+    print("incorrect")
+answer=input("What is the most common surname in the United States?")
+if answer=="Smith":
+    print("correct!")
+else:
+    print("incorrect")
+answer=input("Who wrote the novel Pride and Prejudice?")
+if answer==" Jane Austen":
+    print("correct!")
+else:
+    print("incorrect")
+answer=input("What is the chemical symbol for gold?")
+if answer=="Au":
+    print("correct!")
+else:
+    print("incorrect")
+
+
